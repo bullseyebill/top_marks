191da02f558f4ddb3de5fbb9259128f48409c598
Rupert <rupertjhewson@gmail.com>
2020-04-28 14:48:22 +0000

    typo

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-app = Flask(__name_)
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -10,6 +10,6 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('0.0.0.0'),
-            port=int(os.environ.get('5000')),
+    app.run(host=os.environ.get('IP','0.0.0.0'),
+            port=int(os.environ.get('PORT','5000')),
             debug=True)  
